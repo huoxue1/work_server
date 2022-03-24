ec04bb409c447508dc4c1fa4f4a53e3cc86a359f
3343780376 <3343780376@qq.com>
2022-03-24 14:40:53 +0800

    修改一键安装脚本

--- docs/start.sh
+++ docs/start.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 echo "正在下载作业管理系统"
-wget https://github.do/https://github.com/huoxue1/work_server/releases/download/v1.0.3/work_server_linux_amd64.tar.gz
+wget https://github.do/https://github.com/huoxue1/work_server/releases/download/v1.0.4/work_server_linux_amd64.tar.gz
 echo "文件下载完毕，开始解压文件"
 tar -xzvf work_server_linux_amd64.tar.gz
 chmod -R 777 ./work_server
